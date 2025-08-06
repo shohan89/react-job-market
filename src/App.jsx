3a9ac6c3267a4930a96f44daac2b816b5d2f0d6e
shoh4n <maffin.killer007@gmail.com>
2025-08-06 11:57:49 +0600

    Setup Job Market: install tailwindcss, daisyUi

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Job Market</h1>
+      <button className="btn btn-primary">Primary</button>
+    </>
+  )
+}
+
+export default App
