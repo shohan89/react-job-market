575f48a5080e46185ebd04f2dc8cf50423a2af42
shoh4n <maffin.killer007@gmail.com>
2025-08-07 16:41:55 +0600

    feat: add filter on applied jobs page

--- src/pages/AppliedJobs.jsx
+++ src/pages/AppliedJobs.jsx
@@ -5,12 +5,27 @@ import { getStoredJobApplication } from "../utils/localStorage";
 const AppliedJobs = () => {
     const jobs = useLoaderData();
     const [appliedJobs, setAppliedJobs] = useState([]);
+    const [displayJobs, setDisplayJobs] = useState([]);
+    const handleJobFilter = filter => {
+        if(filter === 'all'){
+            setDisplayJobs(appliedJobs)
+        }
+        else if(filter === 'remote'){
+            const remoteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Remote')
+            setDisplayJobs(remoteJobs);
+        }
+        else if(filter === 'onsite'){
+            const onsiteJobs = appliedJobs.filter(job => job.remote_or_onsite === "Onsite")
+            setDisplayJobs(onsiteJobs)
+        }
+    }
     useEffect(()=>{
         const storedJobsIds = getStoredJobApplication();
         if(jobs.length){
             const jobApplied = jobs.filter(job => storedJobsIds.includes(job.id))
             // console.log(jobApplied, jobs, storedJobsIds)
             setAppliedJobs(jobApplied);
+            setDisplayJobs(jobApplied);
         }
     },[jobs])
     return (
@@ -20,16 +35,16 @@ const AppliedJobs = () => {
                 <details className="dropdown">
                     <summary className="btn m-1">open or close</summary>
                     <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
-                        <li><a>All</a></li>
-                        <li><a>Remote</a></li>
-                        <li><a>Onsite</a></li>
+                        <li onClick={()=>handleJobFilter('all')}><a>All</a></li>
+                        <li onClick={()=>handleJobFilter('remote')}><a>Remote</a></li>
+                        <li onClick={()=>handleJobFilter('onsite')}><a>Onsite</a></li>
                     </ul>
                 </details>
             </div>
             <hr  />
             <div className="max-w-7xl mx-auto">
                 {
-                    appliedJobs.map(job => <li key={job.id}>Title: <span>{job.job_title} </span>Company Name: <span>{job.company_name} </span><span>Job Type: {job.remote_or_onsite}</span></li>)
+                    displayJobs.map(job => <li key={job.id}>Title: <span>{job.job_title} </span>Company Name: <span>{job.company_name} </span><span>Job Type: {job.remote_or_onsite}</span></li>)
                 }
             </div>
         </div>
